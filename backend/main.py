cb5cb18207eeeefeb9591921182f58d0fceb0756
Faruk Gazi <farukgazi@Faruks-MacBook-Air.local>
2025-09-05 21:38:53 +0530

    Fix CORS - allow all origins

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://notes-app-812c9.web.app", "https://*.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
